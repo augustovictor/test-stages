dd3647500b6a84dc76e3af234a929a775f0fb9c6
augustovictor <victoraweb@gmail.com>
2019-07-14 16:35:06 -0300

    [TEST] Add db test

---
+++ src/test/kotlin/augustovictor/com/github/teststages/movie/MovieIntegrationTestWithDB.kt
@@ -0,0 +1,27 @@
+package augustovictor.com.github.teststages.movie
+
+import augustovictor.com.github.teststages.TestStagesApplicationTests
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityManager
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [TestStagesApplicationTests::class])
+@Transactional(propagation = Propagation.NESTED)
+@EnableAutoConfiguration
+class MovieIntegrationTestWithDB {
+    @Autowired
+    private lateinit var entityManager: EntityManager
+
+    @Test
+    fun `should insert data to the movies table`() {
+        Assert.assertTrue(true)
+    }
+}
