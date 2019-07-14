7c3f5686c7c128b23d2b4c4f54d7a08d99fd9b99
augustovictor <victoraweb@gmail.com>
2019-07-14 17:01:56 -0300

    [CHORE] Add flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.flywaydb:flyway-core")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	runtimeOnly("org.postgresql:postgresql")
 	compile("postgresql:postgresql9.0-801.jdbc4")
